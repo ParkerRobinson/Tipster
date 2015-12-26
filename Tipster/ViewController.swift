327ea423603d799ffa2a38a6eeaeadcf900dec52
probinson1996 <parker.robinson1996@gmail.com>
2015-12-26 17:22:11 -0500

    Initial Commit. Contains the initial Tip Calculator.

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject)
+    {
+        view.endEditing(true)
+    }
 
+    @IBAction func onEditingChanged(sender: AnyObject)
+    {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
